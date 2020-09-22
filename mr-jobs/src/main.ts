939d5e508330b5d18d8d2dfcaee777b4d6d6c16c
Nicolás Arias <josue.arias@epn.edu.ec>
2020-09-21 22:04:12 -0500

    Funcionalidad view home sin diseño

--- mr-jobs/src/main.ts
+++ mr-jobs/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+const express = require('express')
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule) as any;
+  app.set('view engine', 'ejs')
+  app.use(express.static('publico'))
+
+
   await app.listen(3000);
 }
 bootstrap();
--- mr-jobs/src/usuario/usuario.controller.ts
+++ mr-jobs/src/usuario/usuario.controller.ts
@@ -1,15 +1,56 @@
-import {Controller, Get} from "@nestjs/common";
+import {Controller, Get, Query, Res} from "@nestjs/common";
 
-@Controller("usuario")
-
+//http://localhost:3000/home
+@Controller("home")
 export class UsuarioController{
 
 
-    //http://localhost:3001/usuario/prueba
+    //http://localhost:3000/usuario/prueba
     @Get("prueba")
     prueba(){
         return "Retorno de Prueba"
     }
 
+    //http://localhost:3000/home
+    @Get("")
+    async inicio(
+        @Res() res,
+        //@Query() parametrosConsulta
+    ){
+        const controlador = "home";
+        res.render(
+            'usuario/inicio',
+            {
+                controlador: controlador
+            });
+    }
+
+    //http://localhost:3000/home/login
+    @Get("login")
+    async login(
+        @Res() res,
+        //@Query() parametrosConsulta
+    ){
+        const controlador = "login";
+        res.render(
+            'usuario/login',
+            {
+                controlador: controlador
+            });
+    }
+
+    //http://localhost:3000/home/signup
+    @Get("signup")
+    async signup(
+        @Res() res,
+        //@Query() parametrosConsulta
+    ){
+        const controlador = "signup";
+        res.render(
+            'usuario/signup',
+            {
+                controlador: controlador
+            });
+    }
 
 }
